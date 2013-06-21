1768745fed5548594887ecaa943d830e3ce6a9a8
Javier P <jpwebdev76@gmail.com>
2013-06-21 15:43:41 -0500

    Removed extranneous arguments.

--- public/contacts.js
+++ public/contacts.js
@@ -17,15 +17,11 @@ angular
 		$locationProvider.html5Mode(true);
 	})
 	.factory('Contact', function($resource) {
-		// Add name: '@clean' to populate that property when POSTing. @ means it will be
-		//           populated automatically.
 		return $resource('/api/contacts/:name', {name: '@clean'});
 	})
-	.controller('EditCtrl', function ($scope, $resource, $routeParams, Contact, $http, $location) {
+	.controller('EditCtrl', function ($scope, $routeParams, Contact, $http, $location) {
 		$scope.contact = Contact.get({name: $routeParams.name});
 
-		// $save is part of angular.  It is a method of $resource. 
-		// New methods could be defined if you wanted to.
 		$scope.save = function() {
 			$http.post('/api/contacts/' + $routeParams.name, $scope.contact);
 			var newClean = ($scope.contact.name.first + '-' + $scope.contact.name.last).toLowerCase();
@@ -36,6 +32,6 @@ angular
 		$http.delete('/api/contacts/' + $routeParams.name);
 		$location.path('/').replace();
 	})
-	.controller('TableCtrl', function ($scope, $resource, Contact) {
+	.controller('TableCtrl', function ($scope, Contact) {
 		$scope.contacts = Contact.query();
 	});
